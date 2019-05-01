6fa12cd17a26774d3e499d61f2e9537a06061576
ranuseh <rosalyn.anuseh@gmail.com>
2019-05-01 15:55:22 -0700

    Added methods to order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,63 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully added product to your cart"
+      redirect_to order_path
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not add product to cart"
+      flash[:messages] = @order.errors.messages
+      render :new, status: :bad_request
+    end
+  end
+
+  def show
+    order_id = params[:id]
+    @merchants = Merchant.all
+    @merchant_order = @merchants.where(order_id: order_id)
+  end
+
+  def edit
+    order_id = params[:id]
+    @order = Order.find_by(id: order_id)
+    redirect_to orders_path if @order.nil?
+  end
+
+  def update
+    order_id = params[:id]
+    order = Order.find(order_id)
+
+    order.update(order_params)
+    redirect_to order_path(order.id)
+  end
+
+  def destroy
+    order_id = params[:id]
+    order = Order.find_by(id: order_id)
+
+    unless order
+      head :not_found
+      return
+    end
+
+    order.destroy
+
+    redirect_to orders_path
+  end
+end
+
+private
+
+def order_params
+  params.require(:order).permit(:status, :email, :address, :name, :cc_name, :cc_exp, :cc_num)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :products
   resources :merchants, only: %i[index show]
+  resources :orders
 
   get '/auth/github', as: 'github_login'
   get 'auth/:provider/callback', to: 'merchants#create', as: 'auth_callback'
   delete '/logout', to: 'merchants#destroy', as: 'logout'
+
+  
 end
