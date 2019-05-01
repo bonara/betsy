4594587fe5d5d5c14af3a81609dc85b981ac4715
Nidhi <nidhixpatel@gmail.com>
2019-05-01 11:12:19 -0700

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   resources :merchants, only: %i[index show]
+  resources :categories
 
   get '/auth/github', as: 'github_login'
   get 'auth/:provider/callback', to: 'merchants#create', as: 'auth_callback'
