529ee472306dbed655879611985df136d0a09832
ranuseh <rosalyn.anuseh@gmail.com>
2019-05-01 11:22:23 -0700

    Adding migration models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_060949) do
+ActiveRecord::Schema.define(version: 2019_05_01_181833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 2019_05_01_060949) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.string "status"
+    t.string "email"
+    t.string "address"
+    t.string "name"
+    t.string "cc_name"
+    t.date "cc_exp"
+    t.integer "cc_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -35,6 +47,13 @@ ActiveRecord::Schema.define(version: 2019_05_01_060949) do
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "products", "merchants"
