ed82f96ab8e77aa99970521c2c764878a5e81a09
ranuseh <rosalyn.anuseh@gmail.com>
2019-05-01 14:55:35 -0700

    Added order items model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :merchant
   has_many :reviews
   has_and_belongs_to_many :categories
+  has_many :order_items
+  has_many :orders, through: :order_items
 end
