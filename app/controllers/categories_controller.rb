20cec683aadd8186703e581bacff88cda9bf2681
Nidhi <nidhixpatel@gmail.com>
2019-05-01 15:32:18 -0700

    added create and index test for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,5 +4,27 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    category = Category.new(category_params)
+
+    successful = category.save
+    if successful
+      flash[:status] = :success
+      flash[:message] = "successfully saved category #{category.name}"
+      redirect_to categories_path
+    else
+      flash.now[:status] = :error
+      flash.now[:message] = "Could not save category"
+      render :new, status: :bad_request
+    end
   end
+
+  def index
+    @categories = Category.all
+  end
+
+  def category_params
+    return params.require(:category).permit(:name)
+  end
+
+
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 describe CategoriesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe "create" do
+    it "be able to create a new category" do
+      category_data = {
+        category: {
+          name: "New"
+        },
+      }
+
+      expect {
+        post categories_path, params: category_data
+      }.must_change "Category.count", +1
+
+      must_respond_with :redirect
+      must_redirect_to categories_path
+
+    end
+
+    it "sends back bad_request if no name is sent" do
+      category_data = {
+        category: {
+          name: ""
+        }
+      }
+
+      expect(Category.new(category_data[:category])).wont_be :valid?
+
+      expect {
+        post categories_path, params: category_data
+      }.wont_change "Category.count"
+
+      must_respond_with :bad_request
+    end
+
+  end 
+
+  describe "index" do
+    it "renders without crashing" do
+      get categories_path
+      must_respond_with :ok
+    end
+
+    # it "renders even if there are zero categories" do
+    #   Category.destroy_all
+    #   get categories_path
+    #   must_respond_with :ok
+    # end
+
+  end
+
+
 end
