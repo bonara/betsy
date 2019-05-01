a6705fb6ff1a7bfb3ca40ddb121c3a9e4716a028
Nara <naraa_520@yahoo.com>
2019-04-30 22:51:53 -0700

    Adds products and merchants routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
+  resources :merchants, only: %i[index show]
+
+  get '/auth/github', as: 'github_login'
+  get 'auth/:provider/callback', to: 'merchants#create', as: 'auth_callback'
+  delete '/logout', to: 'merchants#destroy', as: 'logout'
 end
