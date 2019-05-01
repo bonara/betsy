bca35f9d3dd0383464a2fb08a2cd26d66b05271d
Nidhi <nidhixpatel@gmail.com>
2019-05-01 10:57:37 -0700

    fixed join table error for category product

--- db/migrate/20190501173934_create_categories_products_join.rb
+++ db/migrate/20190501173934_create_categories_products_join.rb
@@ -1,7 +1,7 @@
 class CreateCategoriesProductsJoin < ActiveRecord::Migration[5.2]
   def change
     create_table :categories_products_joins do |t|
-      t.belongs to :category, index: true
+      t.belongs_to :category, index: true
       t.belongs_to :product, index: true
     end
   end
