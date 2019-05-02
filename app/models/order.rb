f23b9182a6dd2e41c0ee6422dd9d6fbb2a564cd8
kateannnichols <kateannnichols@gmail.com>
2019-05-02 08:16:35 -0700

    update reviews controller and tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
-<<<<<<< HEAD
-    add_column :orders, :email, :string
-    add_column :orders, :address, :string
-    add_column :orders, :name, :string
-    add_column :orders, :cc_name, :string
-    add_column :orders, :cc_exp, :date
-    add_column :orders, :cc_num, :integer
-=======
->>>>>>> d84b5282587a95747e0a7d18416d7bc10d101515
+  add_column :orders, :email, :string
+  add_column :orders, :address, :string
+  add_column :orders, :name, :string
+  add_column :orders, :cc_name, :string
+  add_column :orders, :cc_exp, :date
+  add_column :orders, :cc_num, :integer
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,43 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 describe ReviewsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'new' do
+    it 'sends a success message when a review has been created' do
+      get new_product_review_path(Product.first.id)
+      must_respond_with :success
+    end
+
+  describe 'create' do
+    it "redirects to product's page when saving a review" do
+      Review.count.must_equal 2
+      proc {
+        post product_reviews_path(products(:planetxyz).id), params: {
+          review: {
+            text_review: 'Another review is here.',
+            rating: 2,
+            product_id: products(:planetxyz).id
+          }
+        }
+      }.must_change 'Review.count', 1
+
+      must_respond_with :redirect
+      must_redirect_to product_path(products(:planetxyz).id)
+    end
+
+    it 'does not allow a review not between 1 and 5' do
+      invalid_review_data = {
+        review: {
+          rating: 100,
+          product_id: Product.first.id
+        }
+      }
+      Review.new(invalid_review_data[:review]).wont_be :valid?
+      start_review_count = Review.count
+      post product_reviews_path(invalid_review_data[:review][:product_id]), params: invalid_review_data
+      must_respond_with :bad_request
+      Review.count.must_equal start_review_count
+    end
+  end
 end
