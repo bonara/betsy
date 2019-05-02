d53bd7439faeee663f82d2d51895b985ef4041e7
kateannnichols <kateannnichols@gmail.com>
2019-05-02 10:24:40 -0700

    fixed models order to avoid merge conflict

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
-  add_column :orders, :email, :string
-  add_column :orders, :address, :string
-  add_column :orders, :name, :string
-  add_column :orders, :cc_name, :string
-  add_column :orders, :cc_exp, :date
-  add_column :orders, :cc_num, :integer
+  has_many :order_items
+  has_many :products, through: :order_items
 end
