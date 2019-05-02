d5d541fe8aef42577c5eb84f2c8f7e3b31d45b9e
Nara <naraa_520@yahoo.com>
2019-05-02 15:26:15 -0700

    Adds require login and current user methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,15 @@ class ApplicationController < ActionController::Base
     raise ActionController::RoutingError, 'Not Found'
   end
 
-  private
-
-  def find_user
-    @login_user = Merchant.find_by(id: session[:merchant_id]) if session[:merchant_id]
+  def current_user
+    @current_user ||= Merchant.find_by(id: session[:merchant_id]) if session[:merchant_id]
   end
+
+  def require_login
+    if current_user.nil?
+      flash[:error] = "You must be logged in to view this section"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
 end
