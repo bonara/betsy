3dcb3f595612c0e2e72a67c71b4cb3f4c3c58409
Nara <naraa_520@yahoo.com>
2019-05-02 16:27:53 -0700

    Merchant controller test and merchant fixture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
+  # before_action :require_login
   before_action :find_user
-
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError, 'Not Found'
@@ -12,9 +12,15 @@ class ApplicationController < ActionController::Base
 
   def require_login
     if current_user.nil?
-      flash[:error] = "You must be logged in to view this section"
-      redirect_back(fallback_location: root_path)
+      flash[:error] = 'You must be logged in to view this section'
+      redirect_to root_path
     end
   end
 
+  private
+
+  # This is a helper method that will be removed later
+  def find_user
+    @current_user = Merchant.find_by(id: session[:merchant_id]) if session[:merchant_id]
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,14 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../../config/environment", __FILE__)
-require "rails/test_help"
-require "minitest/rails"require "minitest/reporters"  # for Colorized output
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../config/environment', __dir__)
+require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/reporters' # for Colorized output
 #  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
   ENV,
   Minitest.backtrace_filter
 )
-
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
@@ -21,4 +21,34 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+
+  def setup
+    # Once you have enabled test mode, all requests
+    # to OmniAuth will be short circuited to use the mock authentication hash.
+    # A request to /auth/provider will redirect immediately to /auth/provider/callback.
+    OmniAuth.config.test_mode = true
+  end
+
+  # Test helper method to generate a mock auth hash
+  # for fixture data
+  def mock_auth_hash(merchant)
+    {
+      provider: merchant.provider,
+      uid: merchant.uid,
+      info: {
+        email: merchant.email,
+        username: merchant.username,
+        name: merchant.name
+      }
+    }
+  end
+
+  def perform_login(merchant = nil)
+    merchant ||= Merchant.first
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(merchant))
+    get auth_callback_path(:github)
+
+    merchant
+  end
 end
