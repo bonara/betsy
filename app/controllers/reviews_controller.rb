e302fa526bac8e634e99ec58e087d10c7ce456ec
ranuseh <rosalyn.anuseh@gmail.com>
2019-05-02 18:13:29 -0700

    Added back carts MVC

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = @current_user.id
+    @review.product_id = @product.id
+
+    if @review.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
 end
+
+private
+
+def review_params
+  params.require(:review).permit(:rating, :comment)
+end
+
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,6 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
 
-  def sub_total
-    sum = 0
-    self.order_items.each do |order_item|
-      sum += order_item.total_price
-    end
-    return sum
-  end
-
   private
 
   def update_status
