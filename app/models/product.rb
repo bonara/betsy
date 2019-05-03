5d54ce66ef5be137f037414f6ffe851985f889ac
Nara <naraa_520@yahoo.com>
2019-05-02 22:08:45 -0700

    Adds product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,7 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :order_items
   has_many :orders, through: :order_items
+
+  validates :name, uniqueness: true, presence: true
+  validates :price, presence: true, :numericality => { greater_than: 0 }
 end
