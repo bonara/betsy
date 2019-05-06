612bd1005b75854e58f1d71186071762af564c71
Nara <naraa_520@yahoo.com>
2019-05-06 11:57:18 -0700

    Updates merchant controller messages

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -12,37 +12,41 @@ class MerchantsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github')
-    if merchant
+    @merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github')
+    if @merchant
       # User was found in the database
-      flash[:success] = "Logged in as returning user #{merchant.name}"
+      flash[:status] = :success
+      flash[:result_text] = "Logged in as returning user #{@merchant.name}"
     else
       # User doesn't match anything in the DB
       # Attempt to create a new user
-      merchant = Merchant.build_from_github(auth_hash)
+      @merchant = Merchant.build_from_github(auth_hash)
 
-      if merchant.save
-        flash[:success] = "Logged in as new user #{merchant.name}"
+      if @merchant.save
+        flash[:status] = :success
+        flash[:result_text] = "Logged in as new user #{@merchant.name}"
       else
         # Couldn't save the user for some reason. If we
         # hit this it probably means there's a bug with the
         # way we've configured GitHub. Our strategy will
         # be to display error messages to make future
         # debugging easier.
-        flash[:error] = "Could not create new user account: #{merchant.errors.messages}"
+        flash.now[:status] = :failure
+        flash.now[:result_text] = '"Could not create new user account:'
+        flash[:error] = @merchant.errors.messages
         return redirect_to root_path
       end
     end
 
     # If we get here, we have a valid user instance
-    session[:merchant_id] = merchant.id
+    session[:merchant_id] = @merchant.id
     redirect_to root_path
   end
 
   def destroy
     session[:merchant_id] = nil
-    flash[:success] = 'Successfully logged out!'
-
+    flash[:status] = :success
+    flash[:result_text] = 'Successfully logged out!'
     redirect_to root_path
   end
 
