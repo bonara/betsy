550a364e5ebd0520d365836164efb3a0e732586e
kateannnichols <kateannnichols@gmail.com>
2019-05-06 07:43:12 -0700

    reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Review < ApplicationRecord
   belongs_to :product
+  belongs_to :user, optional: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :text_review, length: { maximum: 1000, too_long: 'Your review must be under 1000 characters.' }
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,17 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 describe ReviewsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'new' do
+    it 'returns success when making a new review' do
+      get new_product_review_path(name: 'Jupiter Tour')
+      must_respond_with :success
+    end
+
+    it "returns not found when a reviewed product doesn't exist" do
+      get new_product_review_path(name: 'Canada Trip')
+      must_respond_with :not_found
+    end
+  end
 end
