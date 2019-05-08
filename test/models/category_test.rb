0d90f7c109d665a9d499dc6464c9404afa6adf05
Nara <naraa_520@yahoo.com>
2019-05-08 10:49:50 -0700

    Updates category model test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,27 @@
-# require "test_helper"
+require "test_helper"
 
-# describe Category do
+describe Category do
+  describe 'validations' do
+    it 'requires a name' do
+      category = Category.new
+      category.valid?.must_equal false
+      category.errors.messages.must_include :name
+    end
+  end
+  describe 'relations' do
+    it 'has a list of products' do
+      moon = products(:moon)
+      category1 = categories(:romantic)
+      category2 = categories(:adventure)
+      moon.categories << category1
+      moon.categories << category2
+      moon.must_respond_to :categories
+      moon.categories.each do |category|
+        category.must_be_kind_of Category
+      end
 
-#   it "must be valid" do
-#     value(categories(:oneway)).must_be :valid?
-#   end
-
-# end
+      expect(moon.categories.length).must_equal 2
+      expect(category1.products.length).must_equal 1
+    end
+  end
+end
