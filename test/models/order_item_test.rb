c2d438741fafb556c8c2f1a4a1a162d8b0caaccc
Nara <naraa_520@yahoo.com>
2019-05-08 15:43:34 -0700

    Updates order_item test

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,9 +1,22 @@
-# require "test_helper"
+require "test_helper"
 
-# describe OrderItem do
-#   let(:order_item) { OrderItem.new }
+describe OrderItem do
+  describe 'relations' do
+    it 'has an order' do
+      order_item = order_items(:order_item_one)
+      order_item.must_respond_to :order
+    end
+    it 'has a product' do
+      order_item = order_items(:order_item_one)
+      order_item.must_respond_to :product
+    end
+  end
 
-#   it "must be valid" do
-#     value(order_item).must_be :valid?
-#   end
-# end
+  describe 'validations' do
+    it 'requires a quantity' do
+      order_item = OrderItem.new
+      order_item.valid?.must_equal false
+      order_item.errors.messages.must_include :quantity
+    end
+  end
+end
