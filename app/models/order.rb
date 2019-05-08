f2b800643e2b273acdf2cd743fba5a06160ef2da
Nidhi <nidhixpatel@gmail.com>
2019-05-08 13:44:49 -0700

    removed total order

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,13 +17,5 @@ class Order < ApplicationRecord
     return sum
   end
 
-  def total
-    sum = 0
-    self.order_items.each do |item|
-      sum = sum + (item.quantity * item.product.price)
-    end
-    return sum
-  end
-
   
 end
