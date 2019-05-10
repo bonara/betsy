fc69acd91104cfe5800b659a42c61ae22ea5c09b
kateannnichols <kateannnichols@gmail.com>
2019-05-10 12:10:49 -0700

    Review test

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -5,19 +5,27 @@ require 'test_helper'
 describe OrderItemsController do
   describe 'update' do
     it 'can update an item quantity' do
+      # - We need to get an instance of an existing order item
+             # use order item fixture 
+      # - We need to define the new quantity: 5
+      # - We need to update that instance of an existing order item with the new quantity defined above
+      # - We need to confirm that it was updated:
+      #    - Check the order item's quantity... it should equal the new quantity defined above: 5
+      # - Check that the controller did all the right things with flash and redirect
+
       order_item_data = {
         order_item: {
           quantity: '5',
           product_id: 1
         }
       }
-      before = OrderItem.count
+      # before = OrderItem.count
 
       expect do
         post order_items_path, params: order_item_data
       end
 
-      expect(OrderItem.count).must_equal before + 1
+      # expect(OrderItem.count).must_equal before + 1
 
       expect(flash[:result_text]).must_include "Successfully updated #{@order_item.id} quantity"
       must_respond_with :ok
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -2,56 +2,90 @@
 
 require 'test_helper'
 
-describe OrderItemsController do
-  describe 'update' do
-    it 'can update an item quantity' do
-      order_item_data = {
-        order_item: {
-          quantity: '5',
-          product_id: 1
-        }
-      }
-      before = OrderItem.count
-
-      expect do
-        post order_items_path, params: order_item_data
-      end
-
-      expect(OrderItem.count).must_equal before + 1
-
-      expect(flash[:result_text]).must_include "Successfully updated #{@order_item.id} quantity"
-      must_respond_with :ok
+describe ReviewsController do
+  let(:product) { Product.first }
+  describe 'new' do
+    it 'can create a new review' do
+      get new_product_review_path(product)
+      must_respond_with :success
     end
   end
 
-  #   describe 'destroy' do
-  #     it 'can delete an order item' do
-  #         order_item_data = {
-  #           order: {
-  #             quantity: '5',
-  #             product_id: 1
-  #           }
-  #         }
+  describe 'create' do
+    it "can save a valid review" do 
+      review_date = {
+        review: {
+          product_id: 5,
+          rating: 1,
+          user_id: 4
+        }
+      }
+      test_review = Review.new(review_data[:review])
+      test_review.must_respond_with :success
+    end
+  
+    describe 'new' do 
+    it 'does not allow a user to leave a review on their own product' do
+      # There is a logged in user
+      user = users(:ada)
+      perform_login(user)
+    
+      #That logged in user has a product 
+      id = product(:Honeymoon).id
 
-  #       before = OrderItem.count
+      # That logged in user submits a review 
+      review_hash = { review: { comment: review.comment, rating: review.rating }}
+    
+      expect {
+        post product_reviews_path(id) params: review_hash
+      }.wont_change 'Review.count'
+    
+      must_redirect_to product_reviews_path
 
-  #       expect do
-  #         post order_item_path, params: order_item_data
-  #       end
-
-  #       expect(OrderItem.count).must_equal before - 1
-
-  #       must_redirect_to root_path
-  #     end
-  #   end
-  #   describe "destroy" do
-  #     it "can destroy an existing product" do
-
-  #       expect {
-  #         delete product_path(product)
-  #       }.must_change('Product.count', -1)
-
-  #       must_redirect_to products_path
-  #     end
-  #   end
+      # Will flash message that you can't review your own products 
+      expect(flash[:message]).must_equal 'You cannot review your own product.'
+    end 
+  end
 end
+
+
+
+
+# #     review_data = {
+# #       review: {
+# #         product_id: 3,
+# #         rating: '5',
+# #         user_id: 1
+# #       }
+# #     }
+
+# #     merchant_id = Product.find_by(id: 3)
+
+# #     expect do
+# #       post create_product_review_path, params: review_data
+# #     end
+
+# #     if merchant_id == 1
+# #       must_respond_with :failure
+
+# #     else
+# #       must_respond_with :success
+# #     end
+# #   end
+# # end
+
+# it 'does not allow a user to leave a review on their own product' do
+#   user = users(:name)
+#   perform_login(user)
+
+#   id = product(:1).id
+#   review_hash = { review: { comment: review.comment, rating: review.rating }}
+
+#   expect {
+#     post product_reviews_path(id) params: review_hash
+#   }.wont_change 'Review.count'
+
+#   must_redirect_to product_reviews_path
+#   expect(flash[:message]).must_equal 'You cannot review your own product.'
+# end 
+# end
